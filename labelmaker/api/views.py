fb6d04676618c680f098855743090a4919abec3d
javecruz <clony90@gmail.com>
2020-10-17 21:36:54 +0200

    added object level permissions in put/delete

--- labelmaker/api/views.py
+++ labelmaker/api/views.py
@@ -1,14 +1,21 @@
 from rest_framework import viewsets, generics, permissions
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from .permissions import IsLabelOwner
 from .models import Label, CustomUser
 from .serializers import LabelSerializer, UserSerializer
 
 
 class LabelView(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['retrieve', "list"]:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated, IsLabelOwner]
+        return [permission() for permission in permission_classes]
 
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
